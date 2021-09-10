51dff04665d7cb7dcec61234a8b16161676f6144
Ümithan Viçe <umitvice@gmail.com>
2021-09-10 16:53:40 +0300

    master

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,7 +1,8 @@
 import { 
     CART_ADD_ITEM, 
     CART_REMOVE_ITEM,
-    CART_SAVE_SHIPPING_ADDRESS
+    CART_SAVE_SHIPPING_ADDRESS,
+    CART_SAVE_PAYMENT_METHOD
  } from '../constants/cartConstants'
 
 export const cartReducer = (state = { cartItems: [], shippingAdress: {} }, action) => {
@@ -34,6 +35,11 @@ export const cartReducer = (state = { cartItems: [], shippingAdress: {} }, actio
             ...state,
             shippingAdress: action.payload,
           }      
+          case CART_SAVE_PAYMENT_METHOD:
+          return {
+            ...state,
+            paymentMethod: action.payload,
+          }    
         default:
             return state
     }
--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
 import { saveShippingAddress } from '../actions/cartActions'
 
 const ShippingScreen = ({ history }) => {
-      const cart = useSelector(state => state.cart)
-      const { shippingAddress } = cart
+    const cart = useSelector((state) => state.cart)
+    const { shippingAddress } = cart
 
     const [address, setAddress] = useState(shippingAddress.address)
     const [city, setCity] = useState(shippingAddress.city)
@@ -22,6 +23,7 @@ const ShippingScreen = ({ history }) => {
     }
 
   return <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
          <Form.Group controlId='address'>
