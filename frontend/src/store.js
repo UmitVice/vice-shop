112bedd539df412628a76bf60f801be097b3ddbd
Ümithan Viçe <umitvice@gmail.com>
2021-09-10 20:21:17 +0300

    master

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { 
-  productListReducer, 
-  productDetailsReducer 
-} from './reducers/productReducers'
+import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { 
   userLoginReducer, 
@@ -12,6 +9,7 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer
  } from './reducers/userReducers'
+ import { orderCreateReducer } from './reducers/orderReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -21,6 +19,7 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  orderCreate: orderCreateReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') 
