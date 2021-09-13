ae8f45b52dfef0b7715307b3faa0ed05f79ff900
Ümithan Viçe <umitvice@gmail.com>
2021-09-13 21:05:21 +0300

    master

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -5,7 +5,14 @@ import {
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_CLEAR,
-  PRODUCT_DETAILS_FAIL
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_DELETE_SUCCESS,
+  PRODUCT_DELETE_FAIL,
+  PRODUCT_CREATE_REQUEST,
+  PRODUCT_CREATE_SUCCESS,
+  PRODUCT_CREATE_FAIL,
+  PRODUCT_CREATE_RESET
 } from '../constants/productConstants'
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -19,8 +26,7 @@ export const productListReducer = (state = { products: [] }, action) => {
         default:
           return state
     }
- }
-
+}
 
 export const productDetailsReducer = (
   state = { product: { reviews: [] } }, 
@@ -38,4 +44,32 @@ export const productDetailsReducer = (
         default:
           return state
     }
- }
+}
+
+export const productDeleteReducer = (state = {}, action) => {
+    switch (action.type) {
+        case PRODUCT_DELETE_REQUEST:
+          return { loading: true }
+        case PRODUCT_DELETE_SUCCESS:
+          return { loading: false, success: true }
+        case PRODUCT_DELETE_FAIL:
+          return {loading: false, error: action.payload }
+        default:
+          return state
+    }
+}
+
+export const productCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+      case PRODUCT_CREATE_REQUEST:
+        return { loading: true }
+      case PRODUCT_CREATE_SUCCESS:
+        return { loading: false, success: true, product: action.payload }
+      case PRODUCT_CREATE_FAIL:
+        return {loading: false, error: action.payload }
+      case PRODUCT_CREATE_RESET:
+        return {}
+      default:
+        return state
+  }
+}
