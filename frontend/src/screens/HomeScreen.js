7a63c68f8c57bf9149ae23b1e69634c1ec140799
Ümithan Viçe <umitvice@gmail.com>
2021-09-15 11:36:38 +0300

    master

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,44 @@
 import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import Message from '../components/Message'
+import Paginate from '../components/Paginate'
 import { listProducts } from '../actions/productActions'
+import ProductCarousel from '../components/ProductCarousel'
 
-const HomeScreen = () => {
+const HomeScreen = ({ match }) => {
+  const keyword = match.params.keyword
+
+  const pageNumber = match.params.pageNumber || 1
+
   const dispatch = useDispatch()
 
-  const productList = useSelector(state => state.productList)
-  const { loading, error, products } = productList
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products, page, pages } = productList
   
   useEffect(() => {
-    dispatch(listProducts())
-  }, [dispatch])
+    dispatch(listProducts(keyword, pageNumber))
+  }, [dispatch, keyword, pageNumber])
 
   return (
     <>
-        <h1>Latest Products</h1>
-        {loading ? (
-          <s />
-        ) : error ? (
-          <Message variant='danger'>{error}</Message> 
-        ) : ( 
+      <Row />
+      {!keyword ? (
+        <ProductCarousel />
+      ) : (
+        <Link to='/' className='btn btn-light'>
+          Go Back
+        </Link>
+      )}
+      <h1>Latest Products</h1>
+      {loading ? (
+        <s />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <>
           <Row>
             {products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
@@ -30,7 +46,13 @@ const HomeScreen = () => {
               </Col>
             ))}
           </Row>
-        )}     
+          <Paginate
+            pages={pages}
+            page={page}
+            keyword={keyword ? keyword : ''}
+          />
+        </>
+      )}
     </>
   )
 }
