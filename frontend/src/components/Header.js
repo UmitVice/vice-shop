f14a6aa50d957004028dfdb8eb9088f422d775b5
Ümithan Viçe <umitvice@gmail.com>
2021-10-07 10:52:59 +0300

    Main page design

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -59,23 +59,6 @@ const Header = () => {
             </Navbar.Collapse>
           </Container>
         </Navbar>
-        
-  <Nav className="justify-content-center" activeKey="/home">
-    <Nav.Item>
-      <Nav.Link href="/home">Active</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link eventKey="link-1">Link</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link eventKey="link-2">Link</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link eventKey="disabled" disabled>
-        Disabled
-      </Nav.Link>
-    </Nav.Item>
-  </Nav>
     </header>
   )
 }
