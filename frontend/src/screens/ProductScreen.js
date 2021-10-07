ca58ab3e1f8eacaac61742f97e5be4b8eb0e3030
Ümithan Viçe <umitvice@gmail.com>
2021-10-07 23:28:28 +0300

    'images'

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -61,7 +61,7 @@ const ProductScreen = ({ history, match }) => {
           <Meta title={product.name} />
           <Row>
               <Col md={6}>
-                <Image src={product.image} alt={product.name} fluid />
+                <Image src={`${window.location.origin}/${Image.name}`}  fluid />
               </Col>
               <Col md={3}>
                   <ListGroup variant='flush'>
