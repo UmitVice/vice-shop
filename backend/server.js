5be530c05afb0cb111c9d5fe24260423883b457a
Ümithan Viçe <umitvice@gmail.com>
2021-10-17 12:41:45 +0300

    Revert "Revert "Revert "'images'"""
    This reverts commit b5e5ed569847521abc0175a2231dd68775ab32ad.

--- backend/server.js
+++ backend/server.js
@@ -32,8 +32,8 @@ app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
-const folder = path.resolve()
-app.use('/uploads', express.static(path.join(folder, '/uploads')))
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
